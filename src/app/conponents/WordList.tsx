d8959f75f43abf257f961faad27632bc6a529166
ryolingo <ryomatsuya04@gmail.com>
2024-10-08 22:27:21 +0900

    WordListの作成

--- src/app/conponents/WordList.tsx
+++ src/app/conponents/WordList.tsx
@@ -1,7 +1,8 @@
 export const wordsList = [
-  "infrastructure",
-  "optimization",
-  "collaboration",
-  "negotiation",
-  "initiative",
+  { word: "accomplish", meaning: "達成する" },
+  { word: "budget", meaning: "予算" },
+  { word: "collaborate", meaning: "協力する" },
+  { word: "delegation", meaning: "委任" },
+  { word: "entrepreneur", meaning: "起業家" },
+  // ...他の単語も同様に
 ];
