5a89a4c0bffdf33b12adf01049c75fc61d3e7287
ryolingo <ryomatsuya04@gmail.com>
2024-11-19 16:10:51 +0900

    hooksの用途ファイル分割

---
+++ src/app/hooks/user/useFetchUserData.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/firebase/firebase"; // Firebase初期化ファイル
+
+export const useFetchUserData = (userId: string) => {
+  const [gameDataList, setGameDataList] = useState<any[]>([]); // ゲームデータリストを保持
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // FirestoreのGameDataコレクションからuserIdに基づいてすべてのゲームデータを取得
+        const gameDataQuery = query(
+          collection(db, "GameData"),
+          where("userId", "==", userId)
+        );
+        const querySnapshot = await getDocs(gameDataQuery);
+        console.log("GameData Query Snapshot:", querySnapshot.docs);
+
+        const allGameData = await Promise.all(
+          querySnapshot.docs.map(async (doc) => {
+            console.log("GameData Doc:", doc.data());
+            const gameData = doc.data();
+
+            console.log(gameData);
+            // 各ゲームのincorrectWordsサブコレクションを取得
+            const incorrectWordsSnapshot = await getDocs(
+              collection(db, "GameData", doc.id, "incorrectWords")
+            );
+            const incorrectWords = incorrectWordsSnapshot.docs.map(
+              (wordDoc) => ({
+                word: wordDoc.data().word,
+                meaning: wordDoc.data().meaning,
+              })
+            );
+
+            return {
+              ...gameData,
+              incorrectWords,
+            };
+          })
+        );
+
+        setGameDataList(allGameData);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        setLoading(false);
+      }
+    };
+
+    if (userId) {
+      fetchData();
+    }
+  }, [userId]);
+
+  return { gameDataList, loading };
+};
