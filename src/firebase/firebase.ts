68ae8a90045834e718aeca4a98d712f088b69f00
ryolingo <ryomatsuya04@gmail.com>
2024-11-27 13:38:38 +0900

    envの追加

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -11,6 +11,7 @@ const firebaseApp = initializeApp({
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 });
 
 export const storage = getStorage(firebaseApp);
